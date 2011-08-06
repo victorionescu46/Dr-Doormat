a64c7ba3527f26ba0140149ef2f97b4c7c67509d
Victor Ionescu <victor@okapidev.com>
2011-08-06 03:40:28 +0300

    clear

--- app/code/local/Wdc/Catalog/Helper/LivingQuarterHelper.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Wdc_Catalog_Helper_LivingQuarterHelper extends Wdc_Catalog_Helper_OptionsHtml
-{
-	
-	
-}	
-
-?>
--- perms.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-chown -R www-data:www-data /srv/doormat/public/
-chown -R root:root /srv/doormat/public/.hg/
