be110cd263b5b8bec14e5da83b2a12d0638fbd91
Victor Ionescu <victor@okapidev.com>
2011-08-08 17:04:07 +0300

    fix

--- social-header.js
+++ social-header.js
@@ -2,18 +2,18 @@ function social_pop_up(social_url, social_window_id){
 	window.open(social_url, social_window_id, 'width=400, height=400, scrollbars=yes');
 }
 
-$(document).ready(function(){
-	$('#fb-share-trigger').click(function(evt){
+jQuery(document).ready(function(){
+	jQuery('#fb-share-trigger').click(function(evt){
 		evt.preventDefault();
 		social_pop_up('http://www.facebook.com/sharer.php?u=http://beta.drdoormat.com/index.html', 'fbsharewindow');
 	});
 
-	$('#tw-share-trigger').click(function(evt){
+	jQuery('#tw-share-trigger').click(function(evt){
 		evt.preventDefault();
 		social_pop_up('http://twitter.com/home?status=Check this out http://beta.drdoormat.com/index.html', 'twsharewindow');
 	});
 
-	$('#newsletter-trigger').click(function(evt){
+	jQuery('#newsletter-trigger').click(function(evt){
 		evt.preventDefault();
 		social_pop_up('http://visitor.r20.constantcontact.com/d.jsp?llr=w6uwdydab&p=oi&m=1103542047670', 'newsletterwindow');
 	});
