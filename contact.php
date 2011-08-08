f238577d02c2e4fc0cdd14b8bdebeeb872d6ef3c
Victor Ionescu <victor@okapidev.com>
2011-08-08 05:22:08 +0300

    google analytics

--- contact.php
+++ contact.php
@@ -34,7 +34,32 @@
        <script src="js/Futura_LT_400-Futura_LT_700.font.js"></script>
        <script type="text/javascript">
        		Cufon.replace('h2, h3'); // Works without a selector engine
-       	</script>
+		</script>
+		<script type="text/javascript">
+
+
+
+ var _gaq = _gaq || [];
+
+_gaq.push(['_setAccount', 'UA-24873417-1']);
+
+_gaq.push(['_trackPageview']);
+
+
+
+ (function() {
+
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+
+		   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+
+		   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+
+		    })();
+
+
+
+</script>
        	<style type="text/css" media="screen">
        	  #ok { display: none; padding:10px;margin-bottom: 10px;}
        	  #ok p {color: green !important;}
